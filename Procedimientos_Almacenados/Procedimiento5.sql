ba0f317d7e1cf8eeb96473f4dffa0809612e3336
Mrx18 <115897516+MaycolRichardClaudioPinedo18@users.noreply.github.com>
2022-12-05 15:05:42 -0500

    Procedimiento5

---
+++ Procedimientos_Almacenados/Procedimiento5.sql
@@ -0,0 +1,20 @@
+-- Ejercicio_5
+-- Crear un procedimiento almacenado para dar de baja a un empleado 
+-- pasándole como parámetro su apellido. 
+
+USE `db_buena`;
+DROP procedure IF EXISTS `Ejercicio5`;
+
+DELIMITER $$
+USE `db_buena`$$
+CREATE PROCEDURE `Ejercicio5`(IN _apell VARCHAR(150))
+BEGIN
+	SET @id_empleado=0;
+    
+    SELECT emp_no INTO @id_empleado FROM empleado WHERE apellido= _apell;
+    
+    update empleado set fecha_alt=(CURRENT_DATE) WHERE emp_no = @id_empleado;
+    
+    SELECT emp_no, apellido, fecha_alt, oficio FROM empleado WHERE emp_no = @id_empleado;
+    
+END$$
